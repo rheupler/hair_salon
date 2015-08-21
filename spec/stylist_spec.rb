9ac1ba975fc91de8e0f9853e32fe0944ba7a5d91
Ryan Heupler <Rheupler@gmail.com>
2015-08-21 09:35:53 -0700

    passing method to save stylist into array and have two identical stylists equal each other

--- spec/stylist_spec.rb
+++ spec/stylist_spec.rb
@@ -6,4 +6,22 @@ describe(Stylist) do
       expect(Stylist.all()).to(eq([]))
     end
   end
+
+  describe('#save') do
+    it('saves the stylist info to the database') do
+      stylist = Stylist.new({:name => "Uncle Buck", :id => nil})
+      stylist.save()
+      expect(Stylist.all()).to(eq([stylist]))
+    end
+  end
+
+  describe('#==') do
+    it("is the same stylist if it has the same  name and id") do
+      stylist1 = Stylist.new({:name => "Marty", :id => nil})
+      stylist2 = Stylist.new({:name => "Marty", :id => nil})
+      expect(stylist1).to(eq(stylist2))
+    end
+  end
+
+
 end
