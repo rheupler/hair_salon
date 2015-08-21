682b4e38206251c80d94463307c1a4f82ff7acab
Ryan Heupler <Rheupler@gmail.com>
2015-08-21 12:04:17 -0700

    add README instructions

---
+++ README.md
@@ -0,0 +1,28 @@
+Hair Salon
+
+An app to help a hair salon keep track of which clients see which stylists. Created by Ryan Heupler
+
+Set Up
+
+Clone this repository
+Install and run bundler
+$ gem install bundler
+$ bundle
+Create the relevant databases by running the following in psql
+
+CREATE DATABASE hair_salon;
+
+\c hair_salon
+
+CREATE TABLE customers (id serial PRIMARY KEY, name varchar, phone varchar, stylist_id int);
+
+CREATE TABLE stylists (id serial PRIMARY KEY, name varchar);
+
+CREATE DATABASE hair_salon_test WITH TEMPLATE hair_salon;
+
+Start Sinatra $ ruby app.rb
+
+Go to 'localhost:4567' in your favorite browser!
+Copyright
+
+MIT 2015
--- spec/customer_spec.rb
+++ spec/customer_spec.rb
@@ -9,14 +9,14 @@ describe(Customer) do
 
   describe('#name') do
     it("tells you the customer's name") do
-      customer = Customer.new({:name => "Sam Sparks", :phone => "555-55-5555", :stylist_id => 1, :id => nil})
-      expect(customer.name()).to(eq("Sam Sparks"))
+      customer = Customer.new({:name => "Joey", :phone => "123-123-1234", :stylist_id => 1, :id => nil})
+      expect(customer.name()).to(eq("Joey"))
     end
   end
 
   describe('#id') do
     it('sets its ID when you save it') do
-      customer = Customer.new({:name => "Sam Sparks", :phone => "555-55-5555", :stylist_id => 1, :id => nil})
+      customer = Customer.new({:name => "Joey", :phone => "123-123-1234", :stylist_id => 1, :id => nil})
       customer.save()
       expect(customer.id()).to(be_an_instance_of(Fixnum))
     end
@@ -24,7 +24,7 @@ describe(Customer) do
 
   describe('#save') do
     it('saves the customer info to the database') do
-      customer = Customer.new({:name => "Sam Sparks", :phone => "555-55-5555", :stylist_id => 1, :id => nil})
+      customer = Customer.new({:name => "Joey", :phone => "123-123-1234", :stylist_id => 1, :id => nil})
       customer.save()
       expect(Customer.all()).to(eq([customer]))
     end
@@ -32,17 +32,17 @@ describe(Customer) do
 
   describe('#==') do
     it("is the same customer if it has the same  name and id") do
-      customer1 = Customer.new({:name => "Sam Sparks", :phone => "555-55-5555", :stylist_id => 1, :id => nil})
-      customer2 = Customer.new({:name => "Sam Sparks", :phone => "555-55-5555", :stylist_id => 1, :id => nil})
+      customer1 = Customer.new({:name => "Joey", :phone => "123-123-1234", :stylist_id => 1, :id => nil})
+      customer2 = Customer.new({:name => "Joey", :phone => "123-123-1234", :stylist_id => 1, :id => nil})
       expect(customer1).to(eq(customer2))
     end
   end
 
   describe(".find") do
     it("returns a customer by its ID number") do
-      customer1 = Customer.new({:name => "Sam Sparks", :phone => "555-55-5555", :stylist_id => 1, :id => nil})
+      customer1 = Customer.new({:name => "Joey", :phone => "123-123-1234", :stylist_id => 1, :id => nil})
       customer1.save()
-      customer2 = Customer.new({:name => "Heather Hill", :phone => "555-55-6666", :stylist_id => 1, :id => nil})
+      customer2 = Customer.new({:name => "Maxine", :phone => "555-55-6666", :stylist_id => 1, :id => nil})
       customer2.save()
       expect(Customer.find(customer2.id())).to(eq(customer2))
     end
@@ -50,7 +50,7 @@ describe(Customer) do
 
   describe("#update") do
     it("lets you update customers in the database") do
-      customer1 = Customer.new({:name => "Sam Sparks", :phone => "555-55-5555", :stylist_id => 1, :id => nil})
+      customer1 = Customer.new({:name => "Joey", :phone => "123-123-1234", :stylist_id => 1, :id => nil})
       customer1.save()
       customer1.update({:name => "Tina"})
       expect(customer1.name()).to(eq("Tina"))
@@ -59,9 +59,9 @@ describe(Customer) do
 
   describe("#delete") do
     it("lets you delete a customer from the database") do
-      customer1 = Customer.new({:name => "Sam Sparks", :phone => "555-55-5555", :stylist_id => 1, :id => nil})
+      customer1 = Customer.new({:name => "Joey", :phone => "123-123-1234", :stylist_id => 1, :id => nil})
       customer1.save()
-      customer2 = Customer.new({:name => "Heather Hill", :phone => "555-55-6666", :stylist_id => 1, :id => nil})
+      customer2 = Customer.new({:name => "Maxine", :phone => "555-55-6666", :stylist_id => 1, :id => nil})
       customer2.save()
       customer1.delete()
       expect(Customer.all()).to(eq([customer2]))
